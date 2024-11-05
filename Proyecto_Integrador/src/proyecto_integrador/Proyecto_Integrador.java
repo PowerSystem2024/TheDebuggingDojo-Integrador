50c4f79f098d69f575bd548508dba251ae9820d9
GonzaloJuarez <gonzaloarieljuarez11@gmail.com>
2024-11-05 18:38:11 -0300

    Creando Proyecto

---
+++ Proyecto_Integrador/src/proyecto_integrador/Proyecto_Integrador.java
@@ -0,0 +1,21 @@
+/*
+    Universidad Técnologica Nacional, Facultad Regional de San Rafael (UTN-FRSR)
+    Proyecto Final Integrador del grupo The Debugging Dojo
+    Integrantes:
+        - Bruni Gabriel
+        - Cattai Leandro
+        - Cizmadija Florencia
+        - Cizmadija Paula
+        - Correa Pablo
+        - Francia Nahuel
+        - Juarez Gonzalo
+        - Schutt Lázaro
+        - Schutt Valentin
+*/
+package proyecto_integrador;
+
+
+
+public class Proyecto_Integrador {
+    
+}
